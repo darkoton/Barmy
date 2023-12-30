6aeb2426a322b367f98ecbf733424e136c675120
Darkoto <74507912+darkoton@users.noreply.github.com>
2023-12-30 12:41:01 +0200

    Add files via upload

---
+++ js/parallax_text.js
@@ -0,0 +1,3 @@
+
+const parallax=document.querySelector('.main__parallax');function parallaxFn(){parallax.style.backgroundPosition=`50% -${window.pageYOffset * 0.3}px`;}
+window.addEventListener('load',parallaxFn);window.addEventListener('scroll',parallaxFn);window.addEventListener('orientationchange',parallaxFn,false);
